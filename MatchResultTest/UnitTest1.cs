1e7f6b0216759a9533c5457e84f15d371bed244e
robinson.kuo <robinson.kuo@onelab.tw>
2025-08-14 13:30:50 +0800

    feat: add test project

---
+++ MatchResultTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace MatchResultTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
